d34ddeb4aac972a6b208c2e04b9541feb0a55b93
Alic Barandica <abarandica1234@gmail.com>
2025-01-03 21:51:26 -0500

    portafolio completado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from "./components/Header";
-import { Home } from "./components/home";
-import { AboutMe } from "./components/aboutMe";
+import { Home } from "./components/Home";
+import { AboutMe } from "./components/AboutMe";
 import { Proyects } from "./components/Proyects";
 import { Tools } from "./components/Tools";
 import { DetailsAboutMe } from "./components/DetailsAboutMe";
